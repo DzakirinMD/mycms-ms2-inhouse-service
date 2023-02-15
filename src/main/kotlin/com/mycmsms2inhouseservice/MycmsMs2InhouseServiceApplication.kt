0f48e9134aa0544854df3a5507b20198090cedcf
DzakirinMD <dzakirinmd@gmail.com>
2023-02-15 16:02:00 +0800

    added core application

---
+++ src/main/kotlin/com/mycmsms2inhouseservice/MycmsMs2InhouseServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.mycmsms2inhouseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MycmsMs2InhouseServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MycmsMs2InhouseServiceApplication>(*args)
+}
