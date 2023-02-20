f0396368f84910ed8d8af0bd72dce14e4dfcdf66
DzakirinMD <dzakirinmd@gmail.com>
2023-02-20 10:22:52 +0800

    Major Refactor on naming. changed from InhouseTransfers to InhouseTransfer

---
+++ src/main/kotlin/com/mycmsms2inhouseservice/configuration/InhouseTransferConfiguration.kt
@@ -0,0 +1,26 @@
+package com.mycmsms2inhouseservice.configuration
+
+import com.mycmsms2inhouseservice.domain.InhouseTransfer
+import com.mycmsms2inhouseservice.repository.InhouseTransferRepository
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class InhouseTransferConfiguration {
+
+    @Bean
+    fun InhouseTransfersInit(inhouseTransferRepository: InhouseTransferRepository) = ApplicationRunner {
+
+        inhouseTransferRepository.save(
+            InhouseTransfer(
+                id = 1L,
+                inhouseTransferTrxId = "init inhouseTransferTrxId",
+                inhouseTransferTrxName = "init inhouseTransferTrxName",
+                inhouseTransferTrxEmailRecipient = "init inhouseTransferTrxEmailRecipient",
+                inhouseTransferTrxQty = 10,
+                inhouseTransferTrxAmount = 10.15
+        )
+        )
+    }
+}
---
+++ src/main/kotlin/com/mycmsms2inhouseservice/domain/InhouseTransfer.kt
@@ -0,0 +1,23 @@
+package com.mycmsms2inhouseservice.domain
+
+import lombok.Data
+import javax.persistence.*
+
+@Data
+@Entity
+@Table(name = "inhouse_transfers")
+class InhouseTransfer(
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long? = null,
+        var inhouseTransferTrxId: String,
+        var inhouseTransferTrxName: String,
+        var inhouseTransferTrxEmailRecipient: String,
+        var inhouseTransferTrxQty: Int,
+        var inhouseTransferTrxAmount: Double
+) {
+        override fun toString(): String {
+                return "InhouseTransferDTO(inhouseTransferTrxId='$inhouseTransferTrxId', inhouseTransferTrxName='$inhouseTransferTrxName', inhouseTransferTrxEmailRecipient='$inhouseTransferTrxEmailRecipient', inhouseTransferTrxQty=$inhouseTransferTrxQty, inhouseTransferTrxAmount=$inhouseTransferTrxAmount)"
+        }
+}
